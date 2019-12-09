f94efc8bf4bb2381240cbac1dcbddda78250566e
Brandon <wandbrandon@gmail.com>
2019-12-09 15:00:56 -0500

    got the database to output with css, yada yada

--- dbhandler.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-dbfile = sqlite3.connect("")
-db = dbfile.cursor()
-
-
--- flowerweb.py
+++ flowerweb.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, g, render_template
+
+#initialize database
+DATABASE = 'flowers2019.db'
+
+#Create the app
 app = Flask(__name__)
+
+#Getting the database (from documentation)
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+#Closing the database (from documentation)
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 
 @app.route("/")
 @app.route("/home")
-def home():
-    return render_template('index.html')
-
-#@app.route("/flowers")
-#def home():
-#    return render_template('flowers.html')
+def index():
+    cur = get_db().cursor()
+    d = cur.execute("SELECT * FROM sightings")
+    #h = cur.execute("PRAGMA table_info(sightings);")
+    return render_template("index.html", data=d)
